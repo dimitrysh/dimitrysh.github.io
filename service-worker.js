f0dd334d62b8347e71b8f40c8f42e9b6668d6b3e
dimitrysh <shvetsov.dmitr@gmail.com>
2019-11-06 09:16:26 +0200

    update pwa code

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,23 @@
 'use strict';
 
-// CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v1';
 
-// CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
+self.addEventListener('install', (event) => {
+  // ServiceWorker installed
 
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
+self.addEventListener('activate', (event) => {
+  // ServiceWorker activated
 
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
+self.addEventListener('fetch', (event) => {
+  // fetch event handler
 
 });
